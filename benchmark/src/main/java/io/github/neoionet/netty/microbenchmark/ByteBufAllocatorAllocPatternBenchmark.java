78d1ca98d14335ff438f928160d61853f04a9b2f
old driver <29225782+laosijikaichele@users.noreply.github.com>
2026-04-08 10:17:34 +0800

    Reduce warmup and measurement iterations in benchmark

--- benchmark/src/main/java/io/github/neoionet/netty/microbenchmark/ByteBufAllocatorAllocPatternBenchmark.java
+++ benchmark/src/main/java/io/github/neoionet/netty/microbenchmark/ByteBufAllocatorAllocPatternBenchmark.java
@@ -50,8 +50,8 @@ import java.util.function.Supplier;
  * This is a modified portion of `io.netty.microbench.buffer.ByteBufAllocatorAllocPatternBenchmark`
  * from the <a href="https://github.com/netty/netty">netty</a> project.
  */
-@Warmup(iterations = 10, time = 10)
-@Measurement(iterations = 10, time = 10)
+@Warmup(iterations = 5, time = 5)
+@Measurement(iterations = 5, time = 5)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @State(Scope.Benchmark)
 @BenchmarkMode(Mode.AverageTime)
