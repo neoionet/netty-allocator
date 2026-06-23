b9cb2fb9aa0ab40f9fe8f81c3b4b048c67d46629
lao <29225782+laosijikaichele@users.noreply.github.com>
2026-06-23 15:14:48 +0800

    Optimize ByteBufAllocatorAllocPatternBenchmark

--- benchmark/src/main/java/io/github/neoionet/netty/microbenchmark/ByteBufAllocatorAllocPatternBenchmark.java
+++ benchmark/src/main/java/io/github/neoionet/netty/microbenchmark/ByteBufAllocatorAllocPatternBenchmark.java
@@ -120,9 +120,9 @@ public class ByteBufAllocatorAllocPatternBenchmark {
            "1024", // 1K buffers per thread
            "4096", // 4K buffers per thread
            "8192", // 8K buffers per thread
-            8192 * 2 + "", // 16K buffers per thread
-            8192 * 4 + "", // 32K buffers per thread
-            8192 * 8 + "", // 64K buffers per thread
+           "16384", // 16K buffers per thread
+           "32768", // 32K buffers per thread
+           "65536", // 64K buffers per thread
     })
     public int MAX_LIVE_BUFFERS;
 
