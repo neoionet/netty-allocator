3a070734a75ca52fea7f3e4aa8c5fef38eb7221e
lao <29225782+laosijikaichele@users.noreply.github.com>
2026-09-20 08:52:34 +0800

    Code cleanup

--- mimalloc/src/main/java/io/github/neoionet/netty/mimalloc/MiMallocByteBufAllocator.java
+++ mimalloc/src/main/java/io/github/neoionet/netty/mimalloc/MiMallocByteBufAllocator.java
@@ -2274,7 +2274,7 @@ final class MiMallocByteBufAllocator {
         }
         if (heapCollected && System.nanoTime() - heap.lastSegmentsDecayNano > MIN_SEGMENTS_DECAY_NANOS_INTERVAL) {
             Segment segment;
-            int n = Math.min((heap.coldReservedSegmentCount + 1) >>> 1, 8);
+            int n = (heap.coldReservedSegmentCount + 1) >>> 1;
             while (n-- > 0 && (segment = heap.reservedNormalSegmentDeque.pollLast()) != null) {
                 segment.deallocate();
             }
